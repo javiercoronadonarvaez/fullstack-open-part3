11d8b1933aa1412b0aa09743f3b888b6febc5100
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-12 12:54:33 -0600

    Finished Exercise 3.12 from part3

---
+++ mongo.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+//const url = `mongodb+srv://fullstack:${8cEDMoG0sUd61q0U}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://javiercoronarv:${password}@fullstack-open-jcn.8fv7i.mongodb.net/phonebook?retryWrites=true&w=majority&appName=fullstack-open-jcn`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+if (process.argv.length > 3) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+  const user = new User({
+    name: name,
+    number: number,
+  });
+  user.save().then((result) => {
+    console.log(`Added ${user.name} with number ${user.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  User.find({}).then((persons) => {
+    console.log("Phonebook:");
+    persons.forEach((person) => console.log(`${person.name} ${person.number}`));
+    mongoose.connection.close();
+  });
+}
+
+// saveNotesList(notesList);
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
